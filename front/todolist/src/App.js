b1af51674b663a34607ea3c0d1f63e9f28c05d29
moonpeter <moonpeter@kakao.com>
2020-12-07 18:10:54 +0900

    update 12/07

--- front/todolist/src/App.js
+++ front/todolist/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import TodoListTemplate from "./components/js/TodoListTemplate";
+import Form from './components/js/Form'
+import TodoItemList from "./components/js/TodoItemList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <TodoListTemplate form={<Form />}>
+                <TodoItemList/>
+            </TodoListTemplate>
+        );
+    }
 }
 
 export default App;
---
+++ front/todolist/src/components/js/TodoItemList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import TodoItem from "./TodoItem";
+
+class TodoItemList extends React.Component {
+    render() {
+        const {todos, onToggle, onRemove} = this.props;
+
+        return (
+            <div>
+                <div>
+                    <TodoItem content="TodoItem1"/>
+                    <TodoItem content="TodoItem2"/>
+                    <TodoItem content="TodoItem3"/>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default TodoItemList;
