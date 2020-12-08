c7af30271ba02d6d04b35be440e8f95274669de2
moonpeter <moonpeter@kakao.com>
2020-12-08 19:29:46 +0900

    update 12/08

--- front/todolist/src/App.js
+++ front/todolist/src/App.js
@@ -4,6 +4,18 @@ import Form from './components/js/Form'
 import TodoItemList from "./components/js/TodoItemList";
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        const id = 2;
+        this.state = {
+            input: "",
+            todos: [
+                {id: 0, content: '리액트를 공부하자0', isComplete: false },
+                {id: 1, content: '리액트를 공부하자1', isComplete: true },
+            ]
+        }
+    }
+
     render() {
         return (
             <TodoListTemplate form={<Form />}>
